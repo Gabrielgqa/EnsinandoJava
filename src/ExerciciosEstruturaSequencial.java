8a251b9ac9146209e39fe23d3d2255be6d1d0a61
GabrielGQA <gabrielgqa@gmail.com>
2025-06-30 12:16:04 -0300

    feat: Estrutura sequencial calculaRaio

--- src/ExerciciosEstruturaSequencial.java
+++ src/ExerciciosEstruturaSequencial.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ExerciciosEstruturaSequencial {
     public static void main(String[] args) {
-        somaDoisNumeros();
+        calculaRaio();
     }
 
     public static void somaDoisNumeros(){
@@ -14,5 +14,15 @@ public class ExerciciosEstruturaSequencial {
         sc.close();
 
         System.out.printf("SOMA = %d", A + B);
+    }
+
+    public static void calculaRaio(){
+        double raio;
+        double PI = 3.14159;
+
+        Scanner sc = new Scanner(System.in);
+        raio = sc.nextDouble();
+
+        System.out.printf("A = %.4f", PI * Math.pow(raio, 2));
     }
 }
