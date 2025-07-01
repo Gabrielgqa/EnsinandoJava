1577ea1d1c704a9da93c07b0c40ba62f20372411
GabrielGQA <gabrielgqa@gmail.com>
2025-07-01 09:17:12 -0300

    feat: Estrutura sequencial calculaAreas

--- src/ExerciciosEstruturaSequencial.java
+++ src/ExerciciosEstruturaSequencial.java
@@ -89,4 +89,27 @@ public class ExerciciosEstruturaSequencial {
 
         System.out.printf("VALOR A PAGAR: %.2f", total);
     }
+
+    public static void calcularAreas() {
+        double A, B, C, areaTriangulo, areaCirculo, areaTrapezio, areaQuadrado, areaRetangulo;
+        double PI = 3.14159;
+
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextDouble();
+        B = sc.nextDouble();
+        C = sc.nextDouble();
+
+        areaTriangulo = (A * C) / 2;
+        areaCirculo = PI * Math.pow(C, 2);
+        areaTrapezio = ((A + B) * C) / 2;
+        areaQuadrado = Math.pow(B, 2);
+        areaRetangulo = A * B;
+
+
+        System.out.printf("TRIANGULO: %.3f\n", areaTriangulo);
+        System.out.printf("CIRCULO: %.3f\n", areaCirculo);
+        System.out.printf("TRAPEZIO: %.3f\n", areaTrapezio);
+        System.out.printf("QUADRADO: %.3f\n", areaQuadrado);
+        System.out.printf("RETANGULO: %.3f", areaRetangulo);
+    }
 }
